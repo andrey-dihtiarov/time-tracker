f79ed442bf4de2f4b2dc9737e34d264af397d0de
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2021-04-11 04:03:48 +0300

    NotFound component added

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { ROUTE_HOME } from '../../constants/routes'
+
+import { Wrapper, Link } from './NotFound.styles'
+
+const NotFound = () => (
+  <Wrapper>
+    Sorry, it seems like page does not exists.<Link to={ROUTE_HOME}>Go to home page</Link>
+  </Wrapper>
+)
+
+export default NotFound
---
+++ src/components/NotFound/NotFound.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import { Link as RouterLink } from 'react-router-dom'
+import { colors } from '../../constants/colors'
+
+export const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 32px;
+`
+
+export const Link = styled(RouterLink)`
+  color: ${colors.freeSpeechBlue};
+`
