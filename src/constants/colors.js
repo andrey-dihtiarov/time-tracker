72e692af00fbc8f5ad424ff83da027378470b737
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2021-04-11 04:13:43 +0300

    new colors added

---
+++ src/constants/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  gainsboro: '#DDDDDD',
+  silver: '#BBBBBB',
+  freeSpeechBlue: '#3851CA',
+  darkGray: '#A4A4A4',
+  aliceBlue: '#EAF6FF',
+  irisBlue: '#01BCD5',
+  jazzberryJam: '#BA074E',
+}
